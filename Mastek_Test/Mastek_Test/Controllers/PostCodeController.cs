86efe51cf6b9d1f9b14419095f94edf2ea6b3c4c
satishgithub940 <83502527+satishgithub940@users.noreply.github.com>
2024-01-19 23:23:02 +0530

    project setup

---
+++ Mastek_Test/Mastek_Test/Controllers/PostCodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mastek_Test.Controllers
+{
+    public class PostCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
